73dc13ed7165ff63dcea314bf4b496e30fb5f98f
Trenton Broughton <trenton@devpie.com>
2014-04-21 16:46:08 -0400

    Test example

--- examples/donations.php
+++ examples/donations.php
@@ -1,3 +1,9 @@
 <?php
 
-require '../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
+
+use Kindrid;
+
+$k = new Kindrid('5134be9ac3dbf561a6667ab6', '6HEcePafzNyX0Z2vWrSgYFUktnDjqC3p');
+
+$k->donations();
