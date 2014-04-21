d61592027f309d7dc13e995ae5bd20ce8fb59fde
Trenton Broughton <trenton@devpie.com>
2014-04-21 14:21:52 -0400

    More on the classes

--- src/Kindrid/Donation.php
+++ src/Kindrid/Donation.php
@@ -3,4 +3,12 @@
 namespace Kindrid;
 
 class Donation {
+    public $status      = null;
+    public $designation = null;
+    public $number      = null;
+    public $amount      = null;
+    public $donor_id    = null;
+    public $date        = null;
+    public $id          = null;
+    public $donor       = null;
 }
--- src/Kindrid/Donor.php
+++ src/Kindrid/Donor.php
@@ -4,6 +4,16 @@ namespace Kindrid;
 
 class Donor {
 
+    public $city    = null;
+    public $name    = null;
+    public $zip     = null;
+    public $tags    = array();
+    public $id      = null;
+    public $phone   = null;
+    public $state   = null;
+    public $address = null;
+    public $email   = null;
+
     public function donations() {
 
     }
