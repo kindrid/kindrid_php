dde8785cbc0eb4fb686736e56c360f81be0e8a10
Trenton Broughton <trenton@devpie.com>
2014-04-21 16:36:54 -0400

    toward a working example

--- src/Kindrid/Kindrid.php
+++ src/Kindrid/Kindrid.php
@@ -4,12 +4,28 @@ namespace Kindrid;
 
 class Kindrid {
     const VERSION = '0.1.0';
+    const BASE_URI = "https://kindrid.com/api/v1";
+    public $api_key = null;
+    public $api_secret = null;
 
-    public function donations() {
+    public function __constuct($api_key, $api_secret) {
+        $this->api_key = $api_key;
+        $this->api_secret = $api_secret;
+    }
+
+    public function donations($limit=null, $skip=null) {
+        $response = \Httpful\Request::get(Kindrid::BASE_URI.'/donations')
+        ->authenticateWith($this->api_key, $this->api_secret)
+        ->send();
+        echo $response->body->result;
+        return $response;
+    }
+
+    public function donation($id=null) {
 
     }
 
-    public function donors() {
+    public function donors($limit=null, $skip=null) {
         
     }
 }
