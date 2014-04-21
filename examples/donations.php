7baa0e40361f58e1b8c3e058b7c7203f691fd1be
Trenton Broughton <trenton@devpie.com>
2014-04-21 17:12:17 -0400

    changed namespae

--- examples/donations.php
+++ examples/donations.php
@@ -2,8 +2,6 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-use \Kindrid;
-
-$k = new Kindrid('5134be9ac3dbf561a6667ab6', '6HEcePafzNyX0Z2vWrSgYFUktnDjqC3p');
+$k = new Kindrid\Kindrid('5134be9ac3dbf561a6667ab6', '6HEcePafzNyX0Z2vWrSgYFUktnDjqC3p');
 
 $k->donations();
